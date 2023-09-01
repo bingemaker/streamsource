4a0369980a1542db9f52bc87c0422191e0de2d20
Binge Maker <bingemaker@proton.me>
2023-09-02 01:21:59 +0530

    Improve schema to include ott information

--- types/entry.ts
+++ types/entry.ts
@@ -2,6 +2,15 @@ export type Entry = {
   id: number;
   title: string;
   year: number;
+  otts: [
+    {
+      name: "Netflix" | "Amazon Prime" | "Disney+";
+      countries: string[] | "All";
+      added_date: Date;
+      removed_date: Date | null;
+      free: boolean;
+    }
+  ];
   imdb: {
     id: string;
     url: string;
